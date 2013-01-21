0e2e8cb70f1dd7650eca2f89859b569532633f01
ricardo.nunes <jnunes.ricardo@gmail.com>
2013-01-21 15:33:35 -0200

    Implementação dos messages da página de contato.

--- src/com/site/contato/ContatoController.java
+++ src/com/site/contato/ContatoController.java
@@ -1,5 +1,33 @@
 package com.site.contato;
 
-public class ContatoController {
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
+@ManagedBean(name="contato")
+@RequestScoped
+public class ContatoController {
+	
+	ContatoForm form = new ContatoForm();
+
+	public void enviarMensagem(){
+		
+		String msg = "Contato-- Nome: " + form.getNome() + 
+				" E-mail: " + form.getEmail() + " Mensagem: " + form.getMensagem();
+		
+		FacesMessage fm = new FacesMessage(msg);
+	    FacesContext.getCurrentInstance().addMessage("msg", fm);
+		
+	}
+
+	public ContatoForm getForm() {
+		return form;
+	}
+
+	public void setForm(ContatoForm form) {
+		this.form = form;
+	}
+
+	
 }
---
+++ src/com/site/contato/ContatoForm.java
@@ -0,0 +1,28 @@
+package com.site.contato;
+
+public class ContatoForm {
+
+	String nome;
+	String email;
+	String mensagem;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+}
